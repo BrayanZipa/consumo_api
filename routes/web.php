2c097bf874c8b5531c041c5e6040b1d73b574b1d
Brayan Zipa <zipa.fonseca@gmail.com>
2025-06-20 02:16:03 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PersonajeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('personajes.api'));
 });
 
 Route::resource('personajes', PersonajeController::class)->names('personajes')->only(['index', 'show', 'store', 'edit', 'update']);
